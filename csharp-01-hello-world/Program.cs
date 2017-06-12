54ec92fb27a99c5c28a23bf7779f8df1dedaff03
Logan Kelly <logan.r.kelly@gmail.com>
2017-06-12 16:33:57 -0500

    Created a simple prompt and input app from the Hello World template

---
+++ csharp-01-hello-world/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace csharp01helloworld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please! Type a Message!");
+            Console.WriteLine("You said " + Console.ReadLine());
+        }
+    }
+}
